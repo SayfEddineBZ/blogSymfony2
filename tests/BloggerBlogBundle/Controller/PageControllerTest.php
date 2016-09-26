72da3a2ef2b9d0c2aa94e5242f723fd67fa65d5b
saifeddine <bz.sayf.eddine@gamil.com>
2016-09-26 18:27:30 +0200

    test fonctionnel PageController

--- tests/BloggerBlogBundle/Controller/PageControllerTest.php
+++ tests/BloggerBlogBundle/Controller/PageControllerTest.php
@@ -11,6 +11,18 @@
  *
  * @author sayfeddine
  */
-class PageControllerTest {
-    //put your code here
+namespace Blogger\BlogBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class PageControllerTest extends WebTestCase
+{
+    public function testAbout()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/about');
+
+        $this->assertEquals(1, $crawler->filter('h1:contains("About symblog")')->count());
+    }
 }
